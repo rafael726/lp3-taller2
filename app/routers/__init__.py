0d6b525d27af7247bd3b9a73cd29523937e61ab7
Rafael <jamanoyr@gmail.com>
2025-11-22 12:26:19 -0500

    fix: limpiar el archivo __init__.py, eliminando comentarios innecesarios y organizando las importaciones

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,12 +3,11 @@ Paquete de routers de la API.
 Contiene los endpoints organizados por recurso.
 """
 
-# TODO: Importar los routers para facilitar su uso en main.py
-# from .usuarios import router as usuarios_router
-# from .peliculas import router as peliculas_router
-# from .favoritos import router as favoritos_router
+from . import usuarios, peliculas, favoritos
 
-# TODO: Opcional - Definir __all__ para controlar las exportaciones
-# __all__ = [
-# ]
+__all__ = [
+    "usuarios",
+    "peliculas",
+    "favoritos"
+]
 
